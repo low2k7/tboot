8386260d13ea980841e263b3b9152e57a6695fe4
Darktron <omyp12@gmail.com>
2025-01-09 21:43:55 -0500

    Update install.sh
    updated repo path to folder ccminer

--- install.sh
+++ install.sh
@@ -21,9 +21,9 @@ make_folders() {
 download_files() {
     echo "DOWNLOADING FILES..."
     base_url="https://raw.githubusercontent.com/Darktron/tboot/main"
-    wget -q -O /data/data/com.termux/files/home/ccminer/ccminer "$base_url/ccminer" && echo "Downloaded ccminer"
-    wget -q -O /data/data/com.termux/files/home/ccminer/config.json "$base_url/config.json" && echo "Downloaded config.json"
-    wget -q -O /data/data/com.termux/files/home/ccminer/start.sh "$base_url/start.sh" && echo "Downloaded start.sh"
+    wget -q -O /data/data/com.termux/files/home/ccminer/ccminer "$base_url/ccminer/ccminer" && echo "Downloaded ccminer"
+    wget -q -O /data/data/com.termux/files/home/ccminer/config.json "$base_url/ccminer/config.json" && echo "Downloaded config.json"
+    wget -q -O /data/data/com.termux/files/home/ccminer/start.sh "$base_url/ccminer/start.sh" && echo "Downloaded start.sh"
     wget -q -O /data/data/com.termux/files/home/.termux/boot/autoboot.sh "$base_url/.termux/boot/autoboot.sh" && echo "Downloaded autoboot.sh"
     wget -q -O /data/data/com.termux/files/home/.bashrc "$base_url/.bashrc" && echo "Downloaded .bashrc"
 }
